15446c7286df2ad741acbb439f750c0e903cc226
Egin Pardewan <eginpardewan@gmail.com>
2024-10-23 17:56:19 +0700

    memanggil payactivity

--- app/src/main/java/com/example/activity4/MainActivity.kt
+++ app/src/main/java/com/example/activity4/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.activity4.ui.theme.Activity4Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,27 +18,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             Activity4Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    // Panggil PayActivity di sini, bukan Greeting
+                    PayActivity(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Activity4Theme {
-        Greeting("Android")
     }
 }
